6f30874d47b7313986f5cc9b35146561ec94029b
Dalwadi1 <parmarrutvik1807@gmail.com>
2024-03-18 20:07:49 +0530

    full responsive home and sign up

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { MdOutlineManageAccounts } from "react-icons/md";
 import GoogleFontLoader from 'react-google-font-loader';
 import { TbArrowBigRightLinesFilled } from "react-icons/tb";
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { TiArrowForward } from "react-icons/ti";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -24,7 +24,7 @@ const Home = () => {
             />
             <div className='h-screen bg-slate-500'>
                 {/* navbar */}
-                <div className='container flex justify-between sticky top-3 px-6 mx-auto lg:w-11/12'>
+                <div className='container flex justify-between sticky top-3 px-20 mx-auto lg:w-11/12'>
                     <div className='flex'>
                         <div className='md:w-12 w-10 rounded-full cursor-pointer'>
                             <Link to='/'><img src="./images/R3/logo.png" alt="" /></Link>
@@ -37,8 +37,8 @@ const Home = () => {
                     </button>
 
                 </div>
-                <div className='px-6 container mx-auto flex items-center -m-12 h-screen md:w-11/12'>
-                    <div className='h-auto sm:max-w-96 md:max-w-96'>
+                <div className='px-6 mx-auto md:flex items-center -m-12 h-screen md:w-fit'>
+                    <div className='h-auto md:block sm:max-w-96 md:max-w-96 xs:hidden'>
                         <Swiper
                             spaceBetween={30}
                             centeredSlides={true}
@@ -59,12 +59,40 @@ const Home = () => {
 
                         </Swiper>
                     </div>
-                    <div className='sm:w-auto grow lg:text-center'>
-                        <h1 className='text-2xl lg:text-5xl  text-slate-800 font-extrabold' style={{ fontFamily: 'Josefin Sans' }}>Face Recoginition based <br /> attendance system...</h1>
-                        <h1 className='lg:text-xl text-xs'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />Nesciunt illum enim dolorum sequi voluptas dolore quaerat <br /> pariatur modi nemo consectetur!</h1>
-                        <div className='mt-9 flex lg:justify-center'>
-                            <button className='md:bg-slate-700 rounded-full shadow-2xl lg:p-3 p-2 text-white hover:bg-black  flex mx-0' style={{ fontFamily: 'Josefin Sans' }}>Get's start</button>
-                            <button className='md:bg-slate-700 rounded-full shadow-2xl lg:p-3 p-2 text-white hover:bg-black  flex mx-3' style={{ fontFamily: 'Josefin Sans' }}>Take Attendance</button>
+                    <div className='md:hidden mb-32 mt-10 container mx-auto'>
+                        {/* <img src="./images/R2/image5.png" alt="" />
+                         */}
+                        <Swiper
+                            spaceBetween={30}
+                            centeredSlides={true}
+                            autoplay={{
+                                delay: 2500,
+                                disableOnInteraction: false,
+                            }}
+                            // pagination={{
+                            //     clickable: true,
+                            // }}
+                            navigation={false}
+                            modules={[Autoplay, Navigation]}
+                            className="mySwiper"
+                        >
+                            <SwiperSlide><img src="./images/R2/back.png" className='bg-transparent h-56 items-center' alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="./images/R2/image2.png" className='bg-transparent h-56 w-80' alt="" /></SwiperSlide>
+                            <SwiperSlide><img src="./images/R2/image.png" className='bg-transparent h-56 w-80' alt="" /></SwiperSlide>
+
+                        </Swiper>
+                    </div>
+
+                    <div className='grow flex-col items-center justify-center  md:mt-0 xs:bg-slate-400 xs:py-7 px-3 rounded-t-3xl xs:-mt-32'>
+                        <h1 className='lg:text-5xl md:text-2xl text-slate-800 font-extrabold text-center xs:text-xl' style={{ fontFamily: 'Josefin Sans' }}>Face Recoginition based <br /> attendance system...</h1>
+                        <div className='md:mt-3 '>
+                            <h6 className='flex text-sm'><TiArrowForward fontSize={23} />Utilizes Face API or similar technology to detect and recognize faces accurately.</h6>
+                            <h6 className='flex text-sm'><TiArrowForward fontSize={23} />Automatically records attendance based on facial recognition data.</h6>
+                            <h6 className='flex text-sm'><TiArrowForward fontSize={23} />Implements multi-factor authentication to enhance security and prevent unauthorized access.</h6>
+                        </div>
+                        <div className='md:mt-9 xs:mt-7 flex lg:justify-self-center xs:justify-center'>
+                            <button className='xs:bg-gray-700 md:bg-slate-700 rounded-full shadow-2xl md:p-3 sx:p-2 p-2 xs:text-sm text-white hover:bg-black' style={{ fontFamily: 'Josefin Sans' }}>Get's start</button>
+                            <button className='xs:bg-gray-700 md:bg-slate-700 ml-4 rounded-full shadow-2xl md:p-3 sx:p-2 p-2 xs:text-sm text-white hover:bg-black' style={{ fontFamily: 'Josefin Sans' }}>Take Attendance</button>
                         </div>
                     </div>
 
