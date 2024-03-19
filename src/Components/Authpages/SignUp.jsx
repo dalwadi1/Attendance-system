f1d5dd3d286629391d23a8f86836932c034e1997
Dalwadi1 <parmarrutvik1807@gmail.com>
2024-03-19 06:51:50 +0530

    deploy netlify

--- src/Components/Authpages/SignUp.jsx
+++ src/Components/Authpages/SignUp.jsx
@@ -34,11 +34,11 @@ const SignUp = () => {
                     <form className='w-96'>
                         {/* <img className="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" /> */}
                         <h1 className="xs:text-sm md:text-2xl  mb-3 fw-normal text-white text-center" style={{ fontFamily: 'Josefin Sans' }}>Create Account</h1>
-                        <div className='container flex'>
+                        {/* <div className='container flex'>
                             <button className='bg-slate-600 xs:text-sm md:text-lg w-48 rounded-full p-1 mx-3 text-right space-x-10 text-white flex items-center hover:bg-black' style={{ fontFamily: 'Josefin Sans' }}><FcGoogle fontSize={25} /> google</button>
                             <button className='bg-slate-600 xs:text-sm md:text-lg w-48 rounded-full p-1 mx-3 text-right space-x-10 text-white flex items-center hover:bg-black' style={{ fontFamily: 'Josefin Sans' }}><BsFacebook fontSize={25} color='blue' /> facebook</button>
                         </div>
-                        <h5 className='text-center text-white my-2 xs:text-sm md:text-lg' style={{ fontFamily: 'Josefin Sans' }}>OR</h5>
+                        <h5 className='text-center text-white my-2 xs:text-sm md:text-lg' style={{ fontFamily: 'Josefin Sans' }}>OR</h5> */}
                         <div className="form-floating mt-4">
                             <input type="email" className="form-control text-white bg-transparent" id="floatingInput" placeholder="name@example.com" />
                             <label for="floatingInput" style={{ fontFamily: 'Josefin Sans' }}>User Name</label>
@@ -51,11 +51,14 @@ const SignUp = () => {
                             <input type="password" className="form-control text-white bg-transparent text-xl" id="floatingPassword" placeholder="Password" />
                             <label for="floatingPassword" style={{ fontFamily: 'Josefin Sans' }}>Password</label>
                         </div>
+                        <div className="mt-2">
+                            <input type="file" className="form-control text-white bg-transparent text-xl p-3" />
+                        </div>
 
                         <div className='text-center mt-5'>
                             <button className="bg-slate-700 md:text-lg md:w-96 rounded-full text-white xs:w-32 text-sm py-2 hover:bg-black" type="submit" style={{ fontFamily: 'Josefin Sans' }}>Sign up</button>
                         </div>
-                        <h6 className='mt-2 xs:text-center xs:text-sm md:text-right md:text-lg'>Allready have an account?  <Link>Sign in</Link></h6>
+                        <h6 className='mt-2 xs:text-center xs:text-sm md:text-right md:text-lg'>Allready have an account?  <Link className='text-white'>Sign in</Link></h6>
                         {/* <p className="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p> */}
                     </form>
                 </div>
